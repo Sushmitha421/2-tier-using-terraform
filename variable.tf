3246fac3e594ad06fc6db1b9d8d6eab22c2196bf
sushmitha <eekshithak334@gmail.com>
2026-04-22 11:51:35 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "location" {
+  default = "japan east"
+}
+
+variable "location_db" {
+ default = "switzerland north"
+  
+}
+variable "resource_group_name" {
+  default = "sushmi"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  default = "Password123!"
+}
+
+variable "mysql_admin" {
+  default = "ordinary"
+}
+
+variable "mysql_password" {
+  default = "StrongPass#123"
+}
