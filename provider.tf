d63e590af59c7ce749862b4f6582214cf9a5aa4f
sushmitha <eekshithak334@gmail.com>
2026-04-23 14:15:53 +0530

    Update provider.tf to remove sensitive data
    Removed sensitive information from provider configuration.

--- provider.tf
+++ provider.tf
@@ -1,8 +1,5 @@
 provider "azurerm" {
   features {}
- subscription_id = "90a3a94a-bedd-4043-8c36-56c5dc"
-  client_id       = "51ce7594-dcf5-4a6c-b8ad-015749"
-  client_secret   = ".ku8Q~BgMObP8qMJhGdb8Lo-Rnunk6q"
-  tenant_id       = "e9f17581-2abe-4748-bd7b-260b6"
+
 }
 
